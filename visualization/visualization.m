a903722ea941b6dd8d6797294112e568f272cb20
Fuli Wang <52630571+Fuli-Wang@users.noreply.github.com>
2025-06-28 18:02:10 +0100

    Update visualization.m

--- visualization/visualization.m
+++ visualization/visualization.m
@@ -9,7 +9,11 @@ num_legs    = 6;
 pair_offset_deg = 15;       % ±7.5° per pair
 
 % Inpu a pose you want to see
-pose_input = [-0.16367666776497108 -0.6596727518850166 423.72335568362746 -7.069073827644413 7.809277820024048 -7.584001227298295];
+pose_input = [-0.16367666776497108 -0.6596727518850166 423.72335568362746 -7.069073827644413 7.809277820024048 -7.584001227298295]; % [X Y Z ]
+pose_input(:, 4:6) = deg2rad(pose_input(:, 4:6));
+R = eul2rotm([pose_input(6), pose_input(5), pose_input(4)], 'ZYX');
+z_platform = pose_input(3);
+T = pose_input(1:3);
 
 % Base side-center angles: 0°, 120°, 240°
 base_pair_centers = deg2rad([0, 120, 240]);
@@ -39,11 +43,6 @@ for center_angle = platform_pair_centers
 end
 theta_top = mod(theta_top, 2*pi);
 theta_top = sort(theta_top);
-
-pose_input(:, 4:6) = deg2rad(pose_input(:, 4:6));
-R = eul2rotm([pose_input(6), pose_input(5), pose_input(4)], 'ZYX');
-z_platform = pose_input(3);
-T = pose_input(1:3);
 
 %% 2) ACTUATOR POINTS in 3D
 %  Here, base_side_radius & platform_side_radius
